d4173f8235cab7eb00cd42ee9ccc0d14b96d893c
César A <A01374257@itesm.mx>
2018-03-04 09:03:48 -0600

    Archivo js de la BD

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.msIndexedDB || window.mozIndexedDB;
 
+if(window.indexedDB) {
+	console.log("Tu navegador soporta base de datos indexedDB");
+	var db = window.indexedDB.open("CustomerDataBase", 2);
+
+	// Success
+	db.onsuccess = function(event) {
+		console.log("Success: Opened Database...");
+	}
+
+	// Error
+	db.onerror = function(event) {
+		console.log("Error: Could not Open Database");
+	}
+
+	request.onupgradeneeded = function(event) {
+		var db = event.target.result;
+
+		if (db.objectStoreNames.contains("clientes")) {
+			db.deleteObjectStore("clientes");
+		} else {
+			var query = db.createObjectStore("clientes", {keyPath:"id", autoIncrement:true});
+
+			query.onsuccess = function(event) {
+				console.log("Success: The table has been created")
+			}
+
+			query.onerror = function(event) {
+				console.log("Error: The table has not been created")
+			}
+		}
+	}
+} else {
+	console.log("Error: Tu navegador no soporta base de datos indexedDB");
+}
